5ea1af417154a53a703d1449b2b1e64024b246a0
alexstyl <1665273+alexstyl@users.noreply.github.com>
2026-03-04 14:14:44 +0700

    Update AvdBuddy to 0.5.0

--- Casks/avdbuddy.rb
+++ Casks/avdbuddy.rb
@@ -1,6 +1,6 @@
 cask "avdbuddy" do
-  version "0.4.1"
-  sha256 "b1b2237a02951ebb03f2a20adce807ace29d8db737b7c7194e753ebf06dda241"
+  version "0.5.0"
+  sha256 "97529e3ae7bbc34e5a88cd11b774e299a8f8d9527b11b4c75842dec044d608b3"
 
   url "https://github.com/alexstyl/avdbuddy/releases/download/#{version}/AvdBuddy-#{version}.dmg"
   name "AvdBuddy"
